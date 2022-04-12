5211784fee10584ba017478618d0258922b1d379
HenriBDev <henribdev@gmail.com>
2022-04-12 13:56:20 -0300

    :sparkles: feat - New command added "fontcolor"
    - Added package "validate-color" for this command's validation
    - Colors can be selected by name and hex code

---
+++ commands/fontcolor.js
@@ -0,0 +1,37 @@
+// Node modules
+const path = require('path');
+
+// Color validator
+const { validateHTMLColorName, validateHTMLColorHex } = require('validate-color');
+
+// pdfStyle methods
+const { setStyleObjProperty, getStyleObjProperty, startMount } = require(`..${path.sep}instances${path.sep}pdfStyle`);
+
+module.exports = {
+    data: {
+        name: 'fontcolor',
+        params: ['<font_color>'],
+        description: "Changes the font color (`<font_color>` can be a name or a hex code, ex: `#E0E0E0` or `lightblue`)"
+    },
+    async execute(messageSent, parameters){
+
+		// Gets the discord message's data
+        const currentChannel = messageSent.channel; 
+
+        // Verifies if sender selected a valid color
+        if(!parameters || (validateHTMLColorName(parameters.join('')) == false && validateHTMLColorHex(parameters[0]) == false)){
+            return await currentChannel.send("Please select a valid color!");
+        }
+        let newFontColor = parameters.join('');
+
+        // Checks if a document is already in the making
+		const { mounting } = require(`..${path.sep}instances${path.sep}pdfStyle`);
+		if(!mounting){
+			startMount();
+		}
+
+        // Sets new font color
+        newFontColor = await setStyleObjProperty("fontColor", newFontColor);
+		return await currentChannel.send(`Font-color selected: **${getStyleObjProperty('fontColor')}**`);
+    }
+}
